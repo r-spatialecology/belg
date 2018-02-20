4b0f6fb669e6cad3e30975a78018f2cac7bbaa28
Nowosad <tupiszakaczki@gmail.com>
2018-02-20 15:03:48 -0500

    cpp code added

---
+++ src/get_boltzmann.cpp
@@ -0,0 +1,89 @@
+#include <RcppArmadillo.h>
+using namespace Rcpp;
+
+// [[Rcpp::depends(RcppArmadillo)]]
+
+int wu_calc(int d, int d_a, int d_b, int x_a, int x_b){
+  int Wu = 0;
+
+  if (d == 0) {
+    if (d_a == d_b) {
+      if (x_a == x_b) {
+        Wu = 1;
+      } else {
+        Wu = 6;
+      }
+    } else {
+      if (x_a == x_b) {
+        Wu = 4;
+      } else {
+        Wu = 12;
+      }
+    }
+  } else {
+    if (d_a == d_b) {
+      if (x_a == x_b) {
+        Wu = 24 * (d - 1) + 18;
+      } else {
+        Wu = 24 * (d - 1) + 30;
+      }
+    } else {
+      if (x_a == x_b) {
+        Wu = 24 * (d - 1) + 24;
+      } else {
+        Wu = 24 * (d - 1) + 36;
+      }
+    }
+  }
+  return(Wu);
+}
+
+// [[Rcpp::export]]
+double get_boltzmann(arma::mat raster, std::string base = "log", bool relative = false){
+  double Res = 0;
+
+  while ((raster.n_rows > 1) && (raster.n_cols > 1)) {
+    int num_r = raster.n_rows - 1;
+    int num_c = raster.n_cols - 1;
+    arma::mat Scaled(num_r, num_c);
+    arma::mat Result(num_r, num_c);
+
+    for (int i = 0; i < num_r; i++) {
+      for (int j = 0; j < num_c; j++) {
+        arma::mat Scaledtmp = raster.submat(i, j, i + 1, j + 1);
+        arma::vec v = vectorise(Scaledtmp);
+
+        Scaled(i, j) = mean(v);
+        int s = arma::sum(v);
+        int maxi = arma::max(v);
+        int mini = arma::min(v);
+
+        double temp = (s - maxi - mini) / 2.0;
+        int x_a = std::floor(temp);
+        int x_b = std::ceil(temp);
+        int d_a = x_a - mini;
+        int d_b = maxi - x_b;
+        int d = std::min(d_a, d_b);
+        int Wu = wu_calc(d, d_a, d_b, x_a, x_b);
+        if (base == "log"){
+          Result(i, j) = log(Wu);
+        } else if (base == "log10"){
+          Result(i, j) = log10(Wu);
+        } else if (base == "log2"){
+          Result(i, j) = log2(Wu);
+        }
+      }
+    }
+    for (int ro = 0; ro < num_r; ro++) {
+      for (int co = 0; co < num_c; co++) {
+        Res += Result(ro, co);
+      }
+    }
+    if (relative == true){
+      break;
+    } else {
+      raster = Scaled;
+    }
+  }
+  return(Res);
+}
