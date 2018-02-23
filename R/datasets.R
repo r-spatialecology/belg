3463fcc113cbe4fc371de3f5a70d63f3d34cee38
Nowosad <tupiszakaczki@gmail.com>
2018-02-23 14:06:12 -0500

    datasets added

---
+++ R/datasets.R
@@ -0,0 +1,11 @@
+#' Complex landscape
+#'
+#' A dataset containing artificial complex landscape
+#'
+"complex_land"
+
+#' Simple landscape
+#'
+#' A dataset containing artificial simple landscape
+#'
+"simple_land"
