0371ce57910aefd9879afe8ec00d95ade1b65035
jn <tupiszakaczki@gmail.com>
2018-02-24 09:36:06 -0500

    add a rasterbrick test

--- tests/testthat/test-raster.R
+++ tests/testthat/test-raster.R
@@ -1,5 +1,9 @@
 context("entropy calc on Raster*")
 
+library(raster)
+stack_land = stack(simple_land, complex_land)
+brick_land = brick(simple_land, complex_land)
+
 test_that("entropy calc on a RasterLayer is correct", {
 
   # calculations are correct
@@ -12,11 +16,19 @@ test_that("entropy calc on a RasterLayer is correct", {
 
 test_that("entropy calc on a RasterStack is correct", {
 
-  stack_land = stack(simple_land, complex_land)
-
   # calculations are correct
   stack_output = get_boltzmann(stack_land, relative = FALSE, base = "log2")
   expect_equal(c(61.063, 160.889), stack_output, tolerance = 1e-3)
+
+  # bad inputs
+  # warnings
+})
+
+test_that("entropy calc on a RasterBrick is correct", {
+
+  # calculations are correct
+  brick_output = get_boltzmann(brick_land, relative = FALSE, base = "log2")
+  expect_equal(c(61.063, 160.889), brick_output, tolerance = 1e-3)
 
   # bad inputs
   # warnings
