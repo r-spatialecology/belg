2219b48ffa3464b42161ff21e405a444b1eb5f5f
nowosad <dukekubas@gmail.com>
2019-09-19 10:34:31 +0200

    improve exisiting datasets

--- R/datasets.R
+++ R/datasets.R
@@ -1,11 +1,23 @@
+#' Complex landscape (small)
+#'
+#' A dataset containing small artificial complex landscape
+#'
+"complex_land"
+
+#' Simple landscape (small)
+#'
+#' A dataset containing small artificial simple landscape
+#'
+"simple_land"
+
 #' Complex landscape
 #'
 #' A dataset containing artificial complex landscape
 #'
-"complex_land"
+"land_gradient1"
 
 #' Simple landscape
 #'
 #' A dataset containing artificial simple landscape
 #'
-"simple_land"
+"land_gradient2"
